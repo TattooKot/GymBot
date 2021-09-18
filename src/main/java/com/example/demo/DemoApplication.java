f47b6a0d171bab74a9bd69222d27b3d17cd28c65
TattooKot <kotsagastas@gmail.com>
2021-09-18 19:08:32 +0300

    Added new method: addVisit(1)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		Ping.t.start();
+//		Ping.t.start();
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
